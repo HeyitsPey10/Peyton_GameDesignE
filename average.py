76948c9ba2a1a9da99044d36c85be9b3ee2cd571
HeyitsPey10 <jacobepeyton@gmail.com>
2022-01-14 10:13:47 -0600

    Today's Class 1/14/22

---
+++ average.py
@@ -0,0 +1,21 @@
+#Peyton Jacobe
+#Jan 14
+#Declare variables, print variables, print type of data, learn some operators 
+#Program is average of 3 tests 
+#Declare and assign values 
+test1=89
+test2=78.5
+test3=86
+Flag=False
+#to display things on the screen we use the function print()
+# print(type(test1), type(test2), type(Flag))
+#to clear terminal:
+#declare sum to add tests symbol for addition is +
+#highlight and control forward slash to make anything into a comment
+Sum = test1 + test2 + test3
+#average uses division
+Average= Sum/3
+print(Average)
+print("The average of 3 tests is", Average)
+print("Test1 =", test1, end=" ")
+print("Test2 =", test2, end=" ")
