1efba28394709e70cb12f37071f5a2186935efc1
HeyitsPey10 <jacobepeyton@gmail.com>
2022-01-21 09:21:18 -0600

    update

---
+++ printing practice.py
@@ -0,0 +1,20 @@
+#Peyton Jacobe, 1/21/22 
+#Printing practice
+
+import os
+os.system('cls')
+
+#1
+print('Greenhill : 10, Portal : 5.33')
+print('Total students : 240, Boys : 120')
+print('031')
+print('3.561E+02')
+
+
+#2
+print('Welcome to ', end="")
+print('Greenhill School\n')
+
+
+#3
+print("I\nlove\t\Greenhill")
