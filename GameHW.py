3791b31d54a6ee2b00f6f589d871d7793aed04d0
HeyitsPey10 <jacobepeyton@gmail.com>
2022-01-30 16:07:48 -0600

    update

--- GameHW.py
+++ GameHW.py
@@ -13,56 +13,56 @@ def menu():
     print("##      Select your level         ##")
     print("####################################")
 
-guess=0
-level=0
-gameType=""
-
-
-menu()
-check=True
-while check:
-    gameType=int(gameType)
-    if gameType >0 and gameType< 4:
-        check=False
-    print("\nPlease enter a number between 1 and 3")
-except ValueError:
-    print("sorry, wrong input, try again")
-
-
-
-
-
-
-
 #checking for correct user input 
 menu() #calling the function by name
 check=True
 while check:
     try:
         choice=int(input("Level:  "))
-        check = False
+        if choice>0 and choice<4:
+            check = False
+        else:
+            print("please enter numbers 1-3")
     except ValueError:
         print("Please try again, only enter 1-3")
         
 
 if choice == 1:
     myNumber= random.randint(1,10)
+    level1=10
 elif choice == 2:
-    myNumber= random.randint(1,50)
+    myNumber2= random.randint(1,50)
+    level2=50
 elif choice == 3:
-    myNumber= random.randint(1,100)
+    myNumber3= random.randint(1,100)
+    level3=100
 print(choice)
 
 
 myNumber=random.randint(1,10)
 GameOn=True
 while(GameOn):
-    userGuess=int(input("Guess a number from 1-10 "))
+    userGuess=int(input("Guess a number from 1-10"+ str(level1)))
     if myNumber == userGuess:
         print("You got it!")
         GameOn=False
     else:
         print("good guess")
 print("The number was  "+ str(myNumber))
+os.system('cls')
+menu()
+
+
+
+myNumber2=random.randit(1,50)
+GameOn=True
+while(GameOn):
+    userGuess=int(input("Guess a number from 1-50" + str(50)))
+    if myNumber2 == userGuess:
+        print("You got it!")
+        GameOn=False
+    else:
+        print("good guess")
+print("The number was "+ str(myNumber2))
 os.system('cls')
 menu()
