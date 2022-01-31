adafb230171a991ba58171416c19e687bb23a303
HeyitsPey10 <jacobepeyton@gmail.com>
2022-01-31 10:00:12 -0600

    update

---
+++ Stringsss.py
@@ -0,0 +1,15 @@
+#Peyton Jacobe 1/31/22
+
+#Strings are an array of characters. They have many functions
+
+import os
+os.system('cls')
+myName= "Peyton Jacobe"
+myStatement=""" If you use this formatting, it will print
+it exactly
+how you 
+type it, spacing and all"""
+print ("My last name starts with " +myName[7])
+if 'blah' in myStatement:
+    print('true')
+print('expert' not in myStatement)
