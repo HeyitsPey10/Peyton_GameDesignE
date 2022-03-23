0e317c8baaa6622fe9bdd11ccac7a0c465cfa151
HeyitsPey10 <jacobepeyton@gmail.com>
2022-03-23 09:59:57 -0500

    update

---
+++ Mainmenu.py
@@ -0,0 +1,54 @@
+#Peyton Jacobe, 3/23/22
+#Main menu for the game 
+import pygame, os, time
+
+#Variables
+WIDTH=700
+HEIGHT=700
+wb=30
+hb=30
+xs=50
+ys=250
+
+#list messages
+MenuList=['Instructions', 'Settings', 'Level 1', 'Level 2', 'Level 2', 'Level 3', 'Score Board', 'Exit']
+#GET COLORS
+colors={'white':[255,255,255], 'red':[255,0,0], 'aqua':[102,153, 255],
+'orange':[255,85,0],'purple':[48,25,52],'navy':[5,31,64],'pink':[200,3,75]}
+background= colors.get('white')
+randColor=''
+sq_color=colors.get('navy')
+
+#SCREEN
+wind=pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Circle Eats Square")
+
+
+#create diff types of fonts
+TITLE_FNT=pygame.font.SysFont('Verdana', 70)
+MENU_FNT=pygame.font.SysFont('Verdana', 40)
+INST_FNT=pygame.font.SysFont('Verdana', 25)
+
+#Create Title
+text=TITLE_FNT.render('MENU', 1, (5,31,64))
+wind.fill((255,255,255))
+#Get the width of the text
+#x value = width/2 - width of text/2
+xt=WIDTH/2-text.get_width()/2
+wind.blit(text,(xt,50))
+
+#Create First button
+
+
+#Create menu square
+square=pygame.Rect(xs,ys,wb,hb)
+txty=243
+for i in range(7):
+    message=MenuList[i]
+    text=INST_FNT.render(message,1,(48,25,52))
+    wind.blit(text,(90,txty))
+    pygame.draw.rect(wind,sq_color,square)
+    square.y +=50
+    txty+=50
+pygame.display.update()
+pygame.time.delay(10000)
